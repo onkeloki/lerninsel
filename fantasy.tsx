56795f3c265670794b74d978d7bcdd703e7e5f11
Marko Luft <marko.luft@reflact.com>
2021-01-12 21:18:21 +0100

    initial

---
+++ fantasy.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2020.12.18" name="fantasy" tilewidth="32" tileheight="32" tilecount="1064" columns="8">
+ <image source="fantasy.png" width="256" height="4256"/>
+</tileset>
